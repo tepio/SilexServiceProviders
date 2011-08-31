e86bea9de690a5bec18d5ab12d50676c775bf5f0
Andy Cox <a_j_cox@hotmail.com>
2011-08-31 16:45:58 +0100

    Fix path problem compiling on a windows system.

--- SilexExtensions/Compiler/DoctrineCompiler.php
+++ SilexExtensions/Compiler/DoctrineCompiler.php
@@ -47,10 +47,10 @@ class DoctrineCompiler
 
     protected function addFile($phar, $file, $strip = true)
     {
-        $path = str_replace(realpath(__DIR__.'/../..').'/', '', $file->getRealPath());
+        $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, '', $file->getRealPath());
         $content = file_get_contents($file);
         if ($strip) {
-            $content = Kernel::stripComments(file_get_contents($file));
+            $content = Kernel::stripComments($content);
         }
 
         $phar->addFromString($path, $content);
